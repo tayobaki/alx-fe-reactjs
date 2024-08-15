c1d6d0cdc68c37ad570233f35eb0c6fe4b7b61ce
Bakii <tayolowosale@gmail.com>
2024-08-15 10:16:15 +0100

    pushed new directory

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,6 +1,8 @@
 export default function Header() {
   return (
-    <header>
+    <header
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+    >
       <h1>My Favorite Cities</h1>
     </header>
   );
