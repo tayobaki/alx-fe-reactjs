b05c3494556bf3e0d180b00bee56f22f8bf4aa0f
Bakii <tayolowosale@gmail.com>
2024-08-15 12:59:31 +0100

    footer and maincontent changes

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer>
+        <footer style={{ backgroundColor: 'red', padding: '10px' }}>
             <p>© 2023 City Lovers</p>
         </footer>
     )
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MainContent() {
     return (
-        <main>
+        <main style={{ backgroundColor: 'red', padding: '10px' }}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
