a838446e82ebc36d272864779056e83b21446769
Bakii <tayolowosale@gmail.com>
2024-08-15 12:50:19 +0100

    first

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+            <h1>My Favorite Cities</h1>
+        </header>
+    )
+}
