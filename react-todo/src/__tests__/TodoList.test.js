89b1a03d80cabea07ebd1180e0128690b59def7e
Bakii <tayolowosale@gmail.com>
2024-08-29 08:37:41 +0100

    unit test

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 // src/__tests__/TodoList.test.js
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 import TodoList from "../components/TodoList";
 
 describe("TodoList", () => {
