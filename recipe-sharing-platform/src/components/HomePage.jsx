422d81de6a0b80c67c4ca08cd815ea1da6a41648
Bakii <tayolowosale@gmail.com>
2024-09-09 11:48:03 +0100

    second commit

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,16 +12,21 @@ const HomePage = () => {
 
 	return (
 		<div className="container mx-auto p-4">
-			<h1 className="text-3xl font-bold text-center mb-8">Recipe List</h1>
+			<h1 className="text-3xl  font-bold text-center mb-8">Recipe List</h1>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 				{recipes.map((recipe) => (
-					<div key={recipe.id} className="bg-white shadow-md rounded-lg p-4">
+					<div
+						key={recipe.id}
+						className="bg-white hover:shadow-2xl shadow-md rounded-lg p-4"
+					>
 						<img
 							src={recipe.image}
 							alt={recipe.title}
 							className="w-full h-40 object-cover rounded-md"
 						/>
-						<h2 className="text-xl font-semibold mt-4">{recipe.title}</h2>
+						<h2 className="text-xl hover:text-red-300 font-semibold mt-4">
+							{recipe.title}
+						</h2>
 						<p className="text-gray-600 mt-2">{recipe.summary}</p>
 					</div>
 				))}
