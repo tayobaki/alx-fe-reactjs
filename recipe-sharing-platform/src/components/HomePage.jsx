bc2c5c005be9a28037012d91c3e9f745652d0c5c
Bakii <tayolowosale@gmail.com>
2024-09-09 11:55:01 +0100

    first commit

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
 	const [recipes, setRecipes] = useState([]);
@@ -15,7 +16,8 @@ const HomePage = () => {
 			<h1 className="text-3xl  font-bold text-center mb-8">Recipe List</h1>
 			<div className="grid sm:grid-cols-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 				{recipes.map((recipe) => (
-					<div
+					<Link
+						to={`/recipe/${recipe.id}`}
 						key={recipe.id}
 						className="bg-white hover:shadow-2xl shadow-md rounded-lg p-4"
 					>
@@ -28,7 +30,7 @@ const HomePage = () => {
 							{recipe.title}
 						</h2>
 						<p className="text-gray-600 sm:mt-3 mt-2">{recipe.summary}</p>
-					</div>
+					</Link>
 				))}
 			</div>
 		</div>
