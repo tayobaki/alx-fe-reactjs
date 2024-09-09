1c63dc1b47e09df5f66bba3c21986e9b957d687f
Bakii <tayolowosale@gmail.com>
2024-09-09 11:49:22 +0100

    responsive

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
 	return (
 		<div className="container mx-auto p-4">
 			<h1 className="text-3xl  font-bold text-center mb-8">Recipe List</h1>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+			<div className="grid sm:grid-cols-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 				{recipes.map((recipe) => (
 					<div
 						key={recipe.id}
@@ -27,7 +27,7 @@ const HomePage = () => {
 						<h2 className="text-xl hover:text-red-300 font-semibold mt-4">
 							{recipe.title}
 						</h2>
-						<p className="text-gray-600 mt-2">{recipe.summary}</p>
+						<p className="text-gray-600 sm:mt-3 mt-2">{recipe.summary}</p>
 					</div>
 				))}
 			</div>
